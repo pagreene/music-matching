Fix discrepency in argument units.

music.py
@@ -100,19 +100,17 @@ def parse_song_file_name(file_name):
     return {'composer': composer, 'piece': piece, 'performer': performer}
 
 
-def make_shingles(D, L, dur, hop=1):
-    """Make shingles of the array D.
-
-    Note: the units of `L` are "time", which is influence by `dur`. In contrast, 
-    `hop` is in indices, which is independent of `dur`. The equivalence is
-        
-        [L] = dur*[hop],
-    
-    where [entity] indicates the units of entity.
-    """
+def make_shingles(D, L, dur, hop=None):
+    """Make shingles of the array D."""
     N = D.shape[1]
     LL = int(L*N/dur)
-    return np.stack([D[:, (i*hop):(i*hop + LL)] for i in range(0, int((N - LL)/hop) + 1)])
+
+    if hop is None:
+        H = 1
+    else:
+        H = int(hop*N/dur)    
+
+    return np.stack([D[:, (i*H):(i*H + LL)] for i in range(0, int((N - LL)/H) + 1)])
 
 
 def run_experiment(n_samples, song_data, f):